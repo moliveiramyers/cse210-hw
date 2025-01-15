0941ab6d1d913072d1358a74f99f9e885393d346
moliveiramyers <moliveiramyers@byupathway.edu>
2025-01-15 22:30:28 +0000

    Finished loops activity

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        //Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Get a random number from Computer
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        int guess = -1;
+        //Ask user for guess
+        Console.Write("What is the magic number? ");
+
+
+        // Hints for guess
+        do 
+        {   Console.Write("What is the magic number? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } while (guess != number);
+
+
+
+
+
     }
 }
