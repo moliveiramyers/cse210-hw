3c3ea40f5beb68fabccaa877015dff8dba197539
moliveiramyers <moliveiramyers@byupathway.edu>
2025-01-15 20:58:40 +0000

    If statements activity completed.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("What's your grade percentage? ");
+        string valueFromUser = Console.ReadLine();
+
+        int grade = int.Parse(valueFromUser);  
+        int y = 70;
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+            //Console.WriteLine("You got an A+!");
+        }
+        else if (grade < 90 && grade >= 80)
+        {
+            letter = "B";
+            //Console.WriteLine("You got a B! ");
+        }
+        else if (grade < 80 && grade >= 70)
+        {
+            letter = "C";
+            //Console.WriteLine("You got a C! ");
+        }
+        else if (grade < 70 && grade >= 60)
+        {
+            letter = "D";
+            //Console.WriteLine("You got a D! ");
+        }
+        else
+        {
+            letter = "F";
+            //Console.WriteLine("You got a F! :(");
+        }
+        //This if statement will determine whether or not user passed//
+        if (grade >= y)
+        {
+            Console.WriteLine($"Congrats, you passed with grade {letter}! ");
+        }
+        else 
+        {
+            Console.WriteLine("I am sorry! Try harder next time.");
+        }
+
     }
 }
