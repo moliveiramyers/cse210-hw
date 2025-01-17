5a62236396a0a5d9fa4223fc5a7a6fdd2ffd4828
moliveiramyers <moliveiramyers@byupathway.edu>
2025-01-17 10:35:42 +0000

    assigment completed

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,72 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+
+        int userNumber = PromptUserNumber();
+
+        int square = SquareNumber(userNumber);
+
+        DisplayResult(userName, square);
+
+
+
+
+
+
+
+        void DisplayWelcome()
+        {
+
+            Console.WriteLine("Welcome to the program!");        
+
+        }
+
+        
+        static string PromptUserName()
+        {
+
+            Console.WriteLine("Please enter your name: ");
+            string userName = Console.ReadLine();
+
+            return userName;
+
+        }
+
+        static int PromptUserNumber()
+        {
+
+            Console.Write("Please enter your favorite number: ");
+            int UserNumber = int.Parse(Console.ReadLine());
+
+            return UserNumber;
+
+        } 
+        static int SquareNumber(int userNumber)
+        {
+
+            int square = userNumber*userNumber;
+
+            return square;
+
+        }
+      
+       static void DisplayResult(string userName, int square)
+       {
+
+            Console.WriteLine($"{userName}, the square of your number is {square}");
+
+
+       }
+       
+            
+       
     }
 }
